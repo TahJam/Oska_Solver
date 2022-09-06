c650da31773817fadba2b3a03e7a46b01c112125
Taher Jamali <72470092+rehat1@users.noreply.github.com>
2022-09-06 11:45:48 -0700

    Changing the main.py for better readability

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from moveGen import moveGen
 from oskaPlayer import *
-import oskaPlayerGit as git
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     """Compare evaluators"""
     # get the potential moves
     state = convert2D(['---w', 'w-w', 'bb', 'b-w', '---b'])
     curr_player = 'w'
     myMoveGen = moveGen(state, curr_player)
-    gitMoveGen = git.movegen(state, curr_player)
-
+    
     # compare moveGen lists
     print(f'initial state = {convertList(state)}')
-    print(f'{len(myMoveGen)=} and {len(gitMoveGen)=}')
-    for myMove in myMoveGen:
-        if myMove not in gitMoveGen:
-            print(f'{convertList(myMove)} not IN gitMoveGen')
-            for gitMove in gitMoveGen:
-                if gitMove not in myMoveGen:
-                    print(f'{convertList(gitMove)} not IN myMoveGen')
-            break
-    for myMove, gitMove in zip(myMoveGen, gitMoveGen):
-        print(f'myMove={convertList(myMove)}\t\t\tgitMove={convertList(gitMove)}')
+    print(f'{len(myMoveGen)=}')
+    
     # # compare evaluators
     # for myMove in myMoveGen:
     #     print(f'myMove = {convertList(myMove)}')
@@ -68,34 +48,6 @@ if __name__ == '__main__':
     turn=21	black=['-bb-', '---', '--', '--w', 'ww--']
     turn=22	white=['-bb-', '---', '--', '---', 'www-']
     turn=23	black=None
-    Black Won :)
-
-
-    Github game-----------------------
-    first turn	white=['-www', 'w--', '--', '---', 'bbbb']	
-                black=['-www', 'w--', '--', 'b--', '-bbb']
-    turn=0	white=['--ww', 'ww-', '--', 'b--', '-bbb']
-    turn=1	black=['--ww', 'ww-', 'b-', '---', '-bbb']
-    turn=2	white=['---w', 'www', 'b-', '---', '-bbb']
-    turn=3	black=['---w', 'www', 'b-', 'b--', '--bb']
-    turn=4	white=['---w', 'w-w', 'bw', 'b--', '--bb']
-    turn=5	black=['---w', 'w-w', 'bw', 'bb-', '---b']
-    turn=6	white=['---w', 'w-w', 'b-', 'bbw', '---b']
-    turn=7	black=['---w', 'w-w', 'bb', 'b-w', '---b']
-    turn=8	white=['---w', '--w', '-b', 'bww', '---b']
-    turn=9	black=['---w', '-bw', '--', 'bww', '---b']
-    turn=10	white=['---w', '-b-', '-w', 'bww', '---b']
-    turn=11	black=['-b-w', '---', '-w', 'bww', '---b']
-    turn=12	white=['-b--', '--w', '-w', 'bww', '---b']
-    turn=13	black=['-b--', '--w', 'bw', '-ww', '---b']
-    turn=14	white=['-b--', '--w', 'bw', '--w', '-w-b']
-    turn=15	black=['-b--', '-bw', '-w', '--w', '-w-b']
-    turn=16	white=['-b--', '-bw', '--', '-ww', '-w-b']
-    turn=17	black=['-b--', '-bw', '-b', '-w-', '-w--']
-    turn=18	white=['-b--', '-bw', '-b', '---', '-ww-']
-    turn=19	black=['-bb-', '--w', '-b', '---', '-ww-']
-    turn=20	white=['-bb-', '---', '--', '-w-', '-ww-']
-    turn=21	black=None
     Black Won :)
     """
 
@@ -140,24 +92,3 @@ if __name__ == '__main__':
         elif black is None:
             print(f'Black Won :)')
         turn += 1
-
-    print('\n\nGithub game-----------------------')
-    white = git.oskaplayer(['wwww', '---', '--', '---', 'bbbb'], 'w', depth)
-    black = git.oskaplayer(white, 'b', depth)
-    turn = 0
-    print(f'first turn\t{white=}\t{black=}')
-    while white and black:
-        if turn % 2 == 1:  # black turn
-            black = git.oskaplayer(white, 'b', depth)
-            print(f'{turn=}\t{black=}')
-        else:  # white turn
-            white = git.oskaplayer(black, 'w', depth)
-            print(f'{turn=}\t{white=}')
-        # determine the winner (whichever is None won)
-        if white is None:
-            print(f'White Won :)')
-        elif black is None:
-            print(f'Black Won :)')
-        turn += 1
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
